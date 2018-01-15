baeee7997383f316d30b10484c0279659accfb92
hamzaayadiii <haamzaayadii@gmail.com>
2018-01-15 20:19:07 +0100

    edit profile

--- src/AppBundle/Controller/AmisController.php
+++ src/AppBundle/Controller/AmisController.php
@@ -15,8 +15,25 @@ class AmisController extends Controller {
     /**
      * @Route("/", name="accueil")
      */
-    public function indexAction() {
-        return $this->render('accuiel/index.html.twig');
+    public function indexAction(Request $request) {
+        $em = $this->getDoctrine()->getManager();
+        $form = $this->createForm('AppBundle\Form\amisType', null);
+        if ($request->isMethod('POST')) {
+            $amis = $request->request->get('amis')["amis"];
+            $ami = $em->getRepository('AppBundle:User')->find($amis);
+            $currentUserId = $this->get('security.token_storage')->getToken()->getUser()->getId();
+            $currentUser = $em->getRepository('AppBundle:User')->find($currentUserId);
+            $currentUser->addAmi($ami);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($currentUser);
+            $em->flush();
+        }
+        return $this->render('accuiel/index.html.twig', array(
+                    
+                    'form' => $form->createView(),
+        ));
+        
+   
     }
 
     /**
@@ -34,11 +51,35 @@ class AmisController extends Controller {
         $form = $this->createForm('AppBundle\Form\amisType', null);
         if ($request->isMethod('POST')) {
             $amis = $request->request->get('amis')["amis"];
-//dump($amis);die();
             $ami = $em->getRepository('AppBundle:User')->find($amis);
             $currentUserId = $this->get('security.token_storage')->getToken()->getUser()->getId();
             $currentUser = $em->getRepository('AppBundle:User')->find($currentUserId);
             $currentUser->addAmi($ami);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($currentUser);
+            $em->flush();
+        }
+
+       
+        return $this->render('amis/index.html.twig', array(
+                    
+                    'form' => $form->createView(),
+        ));
+    }
+
+     /**
+     * @Route("/amis", name="amis_retirer")
+     */
+    public function retireramisAction(Request $request) {
+        $em = $this->getDoctrine()->getManager();
+        $form = $this->createForm('AppBundle\Form\amisType', null);
+        if ($request->isMethod('GET')) {
+            $amis = $request->query->get('id');
+            $ami = $em->getRepository('AppBundle:User')->find($amis);
+            $currentUserId = $this->get('security.token_storage')->getToken()->getUser()->getId();
+            $currentUser = $em->getRepository('AppBundle:User')->find($currentUserId);
+            if($ami)
+            $currentUser->removeAmi($ami);
             $em = $this->getDoctrine()->getManager();
             $em->persist($currentUser);
             $em->flush();
@@ -51,4 +92,5 @@ class AmisController extends Controller {
         ));
     }
 
+    
 }
