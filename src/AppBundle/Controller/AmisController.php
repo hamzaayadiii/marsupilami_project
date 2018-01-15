1315768248f6fe5fa83272e2973cc6754285cac5
hamzaayadiii <haamzaayadii@gmail.com>
2018-01-15 18:47:39 +0100

    install and setup fosuserbundle

---
+++ src/AppBundle/Controller/AmisController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\FoodRecord;
+use AppBundle\Entity\User;
+use AppBundle\Form\FoodType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Csrf\CsrfToken;
+
+class AmisController extends Controller
+{
+
+    /**
+     * @Route("/", name="accueil")
+     */
+    public function indexAction()
+    {
+        return $this->render('accuiel/index.html.twig');
+    }
+
+    /**
+     * @Route("/amis/ajouter", name="amis_ajouter")
+     */
+    public function ajouteramisAction()
+    {
+        return $this->render('amis/add.html.twig');
+    }
+
+    /**
+     * @Route("/amis", name="amis_index")
+     */
+    public function amisAction(Request $request)
+    {
+
+
+
+        $form = $this->createForm('AppBundle\Form\amisType', null);
+
+if ($request->isMethod('POST')) {
+        $amis = $request->request->get('amis');
+//dump($amis);die();
+      
+$ami = $em->getRepository('AppBundle:User')->find($amis);
+
+$currentUserId= $this->get('security.token_storage')->getToken()->getUser()->getId();
+$currentUser = $em->getRepository('AppBundle:User')->find($currentUserId);
+
+
+     $currentUser 
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+}
+
+        $em = $this->getDoctrine()->getManager();
+        $amis = $em->getRepository('AppBundle:User')->findAll();
+        return $this->render('amis/index.html.twig', array(
+            'amis' => $amis,
+             'form' => $form->createView(),
+        ));
+    }
+
+}
